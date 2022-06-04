f64f6138ba457872363be75fceb83455b591eead
RyXiao <104197199+RyXiao@users.noreply.github.com>
2022-06-04 08:08:36 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+
 
 st.set_page_config(layout="centered", page_title="Printer Eco-calculator")
 st.markdown("## Printer Cost Calculator")
 
+global total_cost, paper_cost, ink_cost, paper_num, cost_perpage
 
 paper_num = st.number_input('Input the page your printed', step=100)
 paper_cost = paper_num * 0.07
@@ -14,16 +18,42 @@ printer_selected = st.selectbox("Please Select Your Printer", printer_list)
 if printer_selected == 'EPSON L4168':
     price = 1599
 
-ink_cost = st.number_input('Input your expenditure of Ink', step=1)
+#
+# def auto_cal():
+#
+#     total_cost = paper_cost + ink_cost + price
+#     cost_perpage = round(total_cost / paper_num, 4)
+
+# ink_cost = st.number_input('Input your expenditure of Ink',value = 289, step=1, on_change=auto_cal)
+ink_cost = st.number_input('Input your expenditure of Ink', value=289, step=1)
 st.write('Ink Expenditure', ink_cost)
 
 if paper_num == 0:
     paper_num = 1
 
 total_cost = paper_cost + ink_cost + price
-st.write('The total cost is ', total_cost)
+total = st.write('The total cost is ', total_cost)
 cost_perpage = round(total_cost / paper_num, 4)
 st.write('The cost of every single page is ', cost_perpage)
 
 
-st.button('Reset')
+
+
+
+if st.button('Reset'):
+    paper_num = 6000
+    ink_cost = 289
+
+j=[]
+
+for i in range(1000, 100000, 100):
+    j.append((i*0.07+price+289/8000*i)/i)
+
+
+chart_data = {"Num":range(1000,100000,100),"Cost":j}
+
+data = pd.DataFrame(chart_data)
+data.head()
+
+fig = px.scatter(data, x="Num", y="Cost")
+st.plotly_chart(fig, use_container_width=True)
